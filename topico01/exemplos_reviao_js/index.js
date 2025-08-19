4c2dc5cb022f9843bded932b26bf3bad290a0478
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2025-08-18 21:05:12 -0300

    Exemplo template literals

--- topico01/exemplos_reviao_js/index.js
+++ topico01/exemplos_reviao_js/index.js
@@ -4,3 +4,9 @@ import exemploNumTypes from "./modules/numTypes.js"
 console.log("Exemplo 01 - TIPOS EM ECMASCRIPT")
 
 exemploNumTypes()
+
+console.log("Exemplo 02 - STRINGS EM ECMASCRIPT")
+
+
+const curso = "Desenvolvimento Frontend II"
+console.log(` Disciplina de ${curso}`);
