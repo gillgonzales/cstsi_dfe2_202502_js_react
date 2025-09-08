091203283609d73077030852b3421e5f88e1ae8c
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2025-09-08 20:02:49 -0300

    feat: novo exemplo alterando o app padrão do vite

--- topico02/04-exemplo-componente-imc/src/App.jsx
+++ topico02/04-exemplo-componente-imc/src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import CardImc from './components/CardImc/CardImc.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CardImc key="p1" pessoa={{ name: 'Fulano', altura: 1.7, peso: 90 }} />
+      <CardImc key="p2" pessoa={{ name: 'Beltrano', altura: 1.8, peso: 75 }} />
+      <CardImc key="p3" pessoa={{ name: 'Cicrano', altura: 1.9, peso: 90 }} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
