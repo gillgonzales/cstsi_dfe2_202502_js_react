4c85089b8645f5ede2e58c164b97e254185aab16
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2025-10-20 15:18:31 -0300

    feat: exemplos de Props Drilling versus useContext

---
+++ topico05/exemplos/06-react-theme-props-drilling/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+import { Home } from './pages/home/Home';
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [theme] = useState('light');
+
+  return (
+    <>
+        <Home setCount={setCount} count={count} theme={theme}/>
+    </>
+  );
+}
+
+export default App;
