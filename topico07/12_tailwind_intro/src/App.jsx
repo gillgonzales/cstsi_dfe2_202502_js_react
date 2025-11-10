f1d086f0e5317c9a9382ac171d592dce8271fb81
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2025-11-10 20:49:02 -0300

    feat: primeiro exemplo tailwind

--- topico07/12_tailwind_intro/src/App.jsx
+++ topico07/12_tailwind_intro/src/App.jsx
@@ -17,18 +17,18 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      {/* <div className="bg-[blue] rounded-full w-[60px] h-[60px] text-white py-5 text-center text-sm mx-auto font-bold"> */}
+      {/* <div className="bg-[blue] mx-auto py-5 rounded-full w-[60px] h-[60px] font-bold text-white text-sm text-center"> */}
       <div className="circle">
         css
       </div>     
-      <div class='bg-blue-500 rounded-full w-[60px] h-[60px] text-white py-5 text-center text-sm mx-auto font-bold' >
+      <div className='bg-blue-500 mx-auto py-5 rounded-full size-[60px] font-bold text-white text-xs text-center'>
         tailwind
       </div>
       <div className="card">
-        <button className="bg-red-500!" onClick={() => setCount((count) => count + 1)}>
+        <button className="bg-red-500"  onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p className="bg-blue-600 rounded-full p-4 mt-2">
+        <p className="bg-blue-600 mt-2 p-4 rounded-full">
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
