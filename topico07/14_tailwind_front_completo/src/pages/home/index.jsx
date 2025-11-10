38a27c2aab02a554d401ba13b51deb517873cffb
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2025-11-10 20:49:57 -0300

    feat: exemplos tailwind componente card e home

--- topico07/14_tailwind_front_completo/src/pages/home/index.jsx
+++ topico07/14_tailwind_front_completo/src/pages/home/index.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="w-full 2xl:max-w-screen-xl">
         <div className="mx-auto grid h-fit min-h-80 gap-x-8 gap-y-5 sm:w-fit sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 2xl:gap-8">
           {!produtos?.get()
-            ? <p>Carregando...</p>
+            ? <p>Carregando...</p>  
             :produtos.get().map((product, i) => (
               <Cards key={product.id*i*Math.ceil(Math.random()*100)} item={product} />
             ))
