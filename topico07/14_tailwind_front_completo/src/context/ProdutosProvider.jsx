12c102badf34a63a70df245e42c8a9c65ca27269
Prof. Gill Gonzales <gillgonzales@ifsul.edu.br>
2025-11-10 21:03:35 -0300

    feat: carregando da api

--- topico07/14_tailwind_front_completo/src/context/ProdutosProvider.jsx
+++ topico07/14_tailwind_front_completo/src/context/ProdutosProvider.jsx
@@ -43,7 +43,8 @@ const ProdutosProvider = ({ children }) => {
     }
     
     useEffect(() => {
-        loadProdutosMock()
+        // loadProdutosMock()
+        loadProdutos();
       }, []);
     
 
