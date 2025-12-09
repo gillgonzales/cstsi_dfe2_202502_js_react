76b48d33b6def1718ca36b776c9212b7c8fa679b
Prof. Gill <gillgonzales@ifsul.edu.br>
2025-12-08 21:20:19 -0300

    refactor: exemplo de uso de cookies e configurações para monorepo laravel

--- topico09/05_axio_laravel_monorepo_cookies/vite.config.js
+++ topico09/05_axio_laravel_monorepo_cookies/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
-w            input: ['resources/js/client/main.jsx'],
+           input: ['resources/js/client/main.jsx'],
             refresh: true,
         }),
         react()
